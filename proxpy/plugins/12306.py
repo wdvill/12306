794dd52c55e5b1ed2ce5cccaa967fba24510db4f
Yu ZHANG <yves.zhang@gmail.com>
2012-11-23 23:10:19 +0800

    Print POST contents

--- proxpy/plugins/12306.py
+++ proxpy/plugins/12306.py
@@ -1,5 +1,17 @@
+from http import * 
+
 def proxy_mangle_request(req):
-    print req
+    host, port = req.getHost()
+    if host.endswith("12306.cn") :
+        # if req.getMethod() == HTTPRequest.METHOD_GET :
+            # print "REQ #%d: GET %s:%d %s" % (req.uid, host, port, req.getPath())
+        if req.getMethod() == HTTPRequest.METHOD_POST :
+            print "************************************************************"
+            print "REQ #%d: POST %s:%d %s" % (req.uid, host, port, req.getPath())
+            # print req.body
+            for (k, v) in req.getParams().items() : 
+                print "\t %s = %s" % (k, v)
+            print "************************************************************"
     return req
 
 def proxy_mangle_response(res):
